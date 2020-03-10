cd8f7c0e5e02c138c6274e8b3e0d67703e9788b1
Erick <erick_rodolfo10@hotmail.com>
2020-03-10 17:47:58 -0300

    Modificação do Exercício

--- Exercício01/Program.cs
+++ Exercício01/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 
-namespace Exercício01
+namespace LP3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            string[] palavras = { "MIAU", "AUAU", "MUUU" };
+            int count = 0;
+
+            foreach (string element in palavras)
+            {
+                count++;
+                Console.WriteLine($"Elemento: {count}: {element}");
+            }
+            Console.WriteLine($"Numbero de elementos: {count}");
         }
     }
-}
