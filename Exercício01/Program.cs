5b8bf525e0b811d182c97fe0feeca48ecee50efb
Erick <erick_rodolfo10@hotmail.com>
2020-03-10 17:34:14 -0300

    Projeto .net

---
+++ Exercício01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercício01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
